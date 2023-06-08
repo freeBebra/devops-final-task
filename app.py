0a2a6172df996f30419192cc4da9ece2f6f87294
Evgeny <shpagin.j@gmail.com>
2023-06-08 16:55:11 +0700

    add duplicate for app.py in root

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, redirect, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_object("project.config.Config")
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+
+@app.route('/')
+def index():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+
+@app.route('/user/add', methods=['POST'])
+def add_user():
+    name = request.form['name']
+    new_user = User(name)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('index'))
